ca74e1f50de15d00233736b60cb632d52bc92905
nishitashewale <144731231+nishitashewale@users.noreply.github.com>
2023-11-28 20:47:45 -0500

    Update 6_Data_Encryption.sql

--- P4_Group_15_Student_Marketplace/Database_Implementation/6_Data_Encryption.sql
+++ P4_Group_15_Student_Marketplace/Database_Implementation/6_Data_Encryption.sql
@@ -27,11 +27,11 @@ WITH ALGORITHM = AES_256
 ENCRYPTION BY CERTIFICATE Certificatepass;
 GO
 
-select * from marketplace.dbo.STUDENT ;
+select * from marketplace_1.dbo.STUDENT ;
 
-ALTER TABLE marketplace.dbo.STUDENT ADD Email_encrypt varbinary(MAX);
-ALTER TABLE marketplace.dbo.STUDENT ADD Phone_encrypt varbinary(MAX);
-ALTER TABLE marketplace.dbo.[USER] ADD Payment_Info_encrypt varbinary(MAX);
+ALTER TABLE marketplace_1.dbo.STUDENT ADD Email_encrypt varbinary(MAX);
+ALTER TABLE marketplace_1.dbo.STUDENT ADD Phone_encrypt varbinary(MAX);
+ALTER TABLE marketplace_1.dbo.[USER] ADD Payment_Info_encrypt varbinary(MAX);
 
 select * from [USER];
 
@@ -40,37 +40,47 @@ select * from [USER];
 /*Encryption Email column*/ -- Run One time only
 OPEN SYMMETRIC KEY Certificatepass_SM DECRYPTION BY CERTIFICATE Certificatepass;
 
-UPDATE marketplace.dbo.STUDENT
+UPDATE marketplace_1.dbo.STUDENT
         SET Email_encrypt = EncryptByKey (Key_GUID('Certificatepass_SM'), Email)
-        FROM marketplace.dbo.STUDENT;
+        FROM marketplace_1.dbo.STUDENT;
         GO
 
 CLOSE SYMMETRIC KEY Certificatepass_SM;
 
-/*-- Drop the old column
-ALTER TABLE marketplace.dbo.Student
+-- Drop the old column
+ALTER TABLE marketplace_1.dbo.Student
 DROP COLUMN Email;
 
 -- Rename the new column to a temporary name
-EXEC sp_rename 'marketplace.dbo.Student.Email_encrypt', 'Email_Temp', 'COLUMN';
+EXEC sp_rename 'marketplace_1.dbo.Student.Email_encrypt', 'Email_Temp', 'COLUMN';
 
 -- Rename the column to the original name
-EXEC sp_rename 'marketplace.dbo.Student.Email_Temp', 'Email', 'COLUMN';
+EXEC sp_rename 'marketplace_1.dbo.Student.Email_Temp', 'Email', 'COLUMN';
 
-
-
-SELECT * FROM STUDENT;*/
+SELECT * FROM STUDENT;
 
 /*Encrypt Phone*/ -- Run One time only
 
 OPEN SYMMETRIC KEY Certificatepass_SM DECRYPTION BY CERTIFICATE Certificatepass;
 
-UPDATE marketplace.dbo.STUDENT 
+UPDATE marketplace_1.dbo.STUDENT 
         SET Phone_encrypt = EncryptByKey (Key_GUID('Certificatepass_SM'), Phone)
-        FROM marketplace.dbo.STUDENT;
+        FROM marketplace_1.dbo.STUDENT;
         GO
 
 CLOSE SYMMETRIC KEY Certificatepass_SM;
+
+-- Drop the old column
+ALTER TABLE marketplace_1.dbo.Student
+DROP COLUMN Phone;
+
+-- Rename the new column to a temporary name
+EXEC sp_rename 'marketplace_1.dbo.Student.Phone_encrypt', 'Phone_Temp', 'COLUMN';
+
+-- Rename the column to the original name
+EXEC sp_rename 'marketplace_1.dbo.Student.Phone_Temp', 'Phone', 'COLUMN';
+
+SELECT * FROM STUDENT;
 
 /**************************************** Phone & Email Column Decryption ************************************/
 
@@ -79,12 +89,13 @@ CLOSE SYMMETRIC KEY Certificatepass_SM;
 OPEN SYMMETRIC KEY Certificatepass_SM
         DECRYPTION BY CERTIFICATE Certificatepass;
 
-SELECT Student_ID, First_Name, Last_Name, Date_of_Birth, Address, Zip_Code, Email AS 'Encrypted Email', Phone AS 'Encrypted Phone',Age ,
-            CONVERT(varchar, DecryptByKey(Email)) AS 'Decrypted Email',CONVERT(varchar, DecryptByKey(Phone)) AS 'Decrypted Phone'
+SELECT Student_ID, First_Name, Last_Name, Date_of_Birth, Address, Zip_Code, Email AS 'Encrypted Email', Phone AS 'Encrypted Phone',
+            CONVERT(varchar, DecryptByKey(Email)) AS 'Decrypted Email', CONVERT(varchar, DecryptByKey(Phone)) AS 'Decrypted Phone'
             FROM STUDENT;
 
 CLOSE SYMMETRIC KEY Certificatepass_SM;
 
+select * from student;
 
 /**************************************** Payment Info Column Encryption & Decryption ***********************************/
 
@@ -92,23 +103,34 @@ CLOSE SYMMETRIC KEY Certificatepass_SM;
 
 OPEN SYMMETRIC KEY Certificatepass_SM DECRYPTION BY CERTIFICATE Certificatepass;
 
-UPDATE marketplace.dbo.[USER]
+UPDATE marketplace_1.dbo.[USER]
         SET Payment_Info_encrypt = EncryptByKey (Key_GUID('Certificatepass_SM'), Payment_Info)
-        FROM marketplace.dbo.[USER];
+        FROM marketplace_1.dbo.[USER];
         GO
 
 CLOSE SYMMETRIC KEY Certificatepass_SM;
+
+-- Drop the old column
+ALTER TABLE marketplace_1.dbo.[USER]
+DROP COLUMN Payment_Info;
+
+-- Rename the new column to a temporary name
+EXEC sp_rename 'marketplace_1.dbo.[USER].Payment_Info_encrypt', 'Payment_Info_Temp', 'COLUMN';
+
+-- Rename the column to the original name
+EXEC sp_rename 'marketplace_1.dbo.[USER].Payment_Info_Temp', 'Payment_Info', 'COLUMN';
+
+SELECT * FROM [USER];
 
 /*Decryption Payment Info Column*/
 
 OPEN SYMMETRIC KEY Certificatepass_SM
         DECRYPTION BY CERTIFICATE Certificatepass;
 
-SELECT Account_ID, User_ID,Shipping_Address,Payment_Info AS 'Encrypted Payment Info' ,
+SELECT Account_ID, User_ID,Shipping_Address,Payment_Info AS 'Encrypted Payment Info',
             CONVERT(varchar, DecryptByKey(Payment_Info)) AS 'Decrypted Payment Info'
             FROM [USER];
 
 CLOSE SYMMETRIC KEY Certificatepass_SM;
 
-
---SELECT * FROM [user];
+SELECT * FROM [user];
